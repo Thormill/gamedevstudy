5fa4bbe5981b3978a6a77fcc001a57cad4ae959a
Anton Shishkin <a.shishkin.b@gmail.com>
2023-03-14 00:14:07 +0300

    Win check

--- tictactoe.cs
+++ tictactoe.cs
@@ -31,6 +31,34 @@ namespace TicTacToe {
     public void Ready() {
       current_turn = true;
     }
+
+    public bool CheckWin(Grid grid) {
+      bool win = false;
+      List<int> marked = new List<int>();
+
+      // grid indexes of marked elements
+      for(int i = 0; i < grid.grid.Length; i++) {
+        if(grid.grid[i].value == figure) {
+          marked.Add(i);
+        }
+      }
+
+      // horizontal lines
+      if (marked.Contains(0) && marked.Contains(1) && marked.Contains(2)) win = true;
+      if (marked.Contains(3) && marked.Contains(4) && marked.Contains(5)) win = true;
+      if (marked.Contains(6) && marked.Contains(7) && marked.Contains(8)) win = true;
+
+      // vertical lines
+      if (marked.Contains(0) && marked.Contains(3) && marked.Contains(6)) win = true;
+      if (marked.Contains(1) && marked.Contains(4) && marked.Contains(7)) win = true;
+      if (marked.Contains(2) && marked.Contains(5) && marked.Contains(8)) win = true;
+
+      // diagonal lines
+      if (marked.Contains(0) && marked.Contains(4) && marked.Contains(8)) win = true;
+      if (marked.Contains(2) && marked.Contains(4) && marked.Contains(6)) win = true;
+
+      return win;
+    }
   }
 
   internal class Field {
@@ -70,7 +98,7 @@ namespace TicTacToe {
   }
 
   internal class Grid {
-    private Field[] grid;
+    public Field[] grid;
 
     public Grid() {
       grid = new Field[9];
@@ -182,6 +210,16 @@ namespace TicTacToe {
             continue;
           }
 
+          // Check win condition
+          if (player.CheckWin(grid)) {
+            grid.Draw();
+            Console.WriteLine("You win!");
+            Console.ReadKey();
+            gameOngoing = false;
+
+            break;
+          }
+
           // Player switch
           player.Turn();
           bot.Ready();
@@ -192,6 +230,15 @@ namespace TicTacToe {
           // AI turn
           int bot_field = rnd.Next(0, free_fields.Length);
           grid.Turn(free_fields[bot_field], bot.Draw());
+
+          if (bot.CheckWin(grid)) {
+            grid.Draw();
+            Console.WriteLine("Bot wins!");
+            Console.ReadKey();
+            gameOngoing = false;
+
+            break;
+          }
 
           // Player switch
           bot.Turn();
